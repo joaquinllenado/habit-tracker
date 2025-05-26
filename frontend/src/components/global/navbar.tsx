be915b87e35c23acf8bc9dfad719a96c22df8a8b
Joaquin Llenado <joaquinllenado@gmail.com>
2025-05-25 17:08:43 -0700

    Conditionally render sign in/sign up buttons

--- frontend/src/components/global/navbar.tsx
+++ frontend/src/components/global/navbar.tsx
@@ -1,13 +1,20 @@
+"use client"
+
 import Link from "next/link"
 import { Button } from "../ui/button"
+import { useUser } from "@/context/UserContext";
 
 export default function Navbar(){
+    const { user } = useUser();
     return(
         <nav className=" w-full h-fit">
             <div className="flex flex-row w-full items-center justify-between px-4 py-2">
                 <Link href='/'>
                     <span className="text-2xl">Habit Tracker</span>
                 </Link>
+                { user ? 
+                <p className="text-lg">Welcome, {user.name}</p>
+                : (
                 <div className="flex flex-row gap-3">
                     <Button variant="ghost" className="cursor-pointer">
                         <Link href='/login'>
@@ -20,6 +27,7 @@ export default function Navbar(){
                         </Link>
                     </Button>
                 </div>
+                )}
             </div>
         </nav>
     )
